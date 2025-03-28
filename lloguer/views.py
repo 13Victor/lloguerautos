9c64e893fb43adc4e339f22a4fcbcbf610064cb4
Victor <vvalerolopez.eb@iesesteveterradas.cat>
2025-03-28 16:45:43 +0100

    [ Fita 5 ] DONE

--- lloguer/views.py
+++ lloguer/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Automobil
 
-# Create your views here.
+def listar_automoviles(request):
+    automoviles = Automobil.objects.all()
+    return render(request, 'lloguer/autos_list.html', {'automoviles': automoviles})
