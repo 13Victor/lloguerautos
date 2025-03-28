c5d89ddf9adf0125a1171db328c8a2f3473b32fe
Victor <vvalerolopez.eb@iesesteveterradas.cat>
2025-03-28 16:08:46 +0100

    [ Fita 3 ] DONE

--- lloguer/admin.py
+++ lloguer/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Automobil
+from .models import Automobil, Reserva
 
 class AutomobilAdmin(admin.ModelAdmin):
     list_display = ('matricula', 'marca', 'model')
 
 admin.site.register(Automobil, AutomobilAdmin)
+
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ('automobil', 'user', 'data_inici', 'data_fi')
+
+admin.site.register(Reserva, ReservaAdmin)
--- lloguer/models.py
+++ lloguer/models.py
@@ -1,9 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Automobil(models.Model):
     marca = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     matricula = models.CharField(max_length=10)
 
+    def __str__(self):
+        return f"{self.matricula} - {self.marca} {self.model}"
+
+class Reserva(models.Model):
+    automobil = models.ForeignKey(Automobil, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    data_inici = models.DateField()
+    data_fi = models.DateField()
+
+    class Meta:
+        unique_together = ('automobil', 'data_inici') 
+
+    def __str__(self):
+        return f"Reserva de {self.automobil} per {self.user} del {self.data_inici} al {self.data_fi}"
